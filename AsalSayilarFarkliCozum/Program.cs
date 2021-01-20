d1c5e7083eece4f1c181df21b309f4423177f947
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-20 17:53:46 +0300

    Add project files.

---
+++ AsalSayilarFarkliCozum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace AsalSayilarFarkliCozum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            int sayac = 0;
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (int J = 1; J <= i; J++)
+                {
+                    if (i % J == 0)
+                    {
+                        sayac++;
+                    }
+                }
+                if (sayac == 2)
+                {
+                    ar.Add(i);
+
+                }
+                sayac = 0;
+            }
+
+            foreach (var x in ar)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
